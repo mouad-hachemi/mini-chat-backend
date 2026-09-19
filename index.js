9f5a45d994551373ea696b917c5094598ac0e4e0
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2026-09-19 15:16:48 +0100

    refactor: extract websocket server init and config to a separate function

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import express from "express";
 import cors from "cors";
-import jsonwebtoken from "jsonwebtoken";
 import cookieParser from "cookie-parser";
-import { WebSocketServer } from "ws";
 import { createServer } from "node:http";
 import authRoutes from "./routes/auth.js";
 import roomsRoutes from "./routes/rooms.js";
 import { authenticateToken } from "./middlewares/authcheck.js";
+import { initServer } from "./services/websocket.js";
 
 const PORT = 8080;
-const JWT_SECRET = process.env.JWT_SECRET || "you-cant-guess-this";
-
-function getTokenFromCookie(cookieHeader) {
-  if (!cookieHeader) return null;
-  const match = cookieHeader.match(/token=([^;]+)/);
-  return match ? match[1] : null;
-}
-
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -39,81 +30,7 @@ app.use(
 );
 
 const server = createServer(app);
-
-const wss = new WebSocketServer({ server: server });
-const rooms = new Map();
-
-wss.on("connection", (ws, req) => {
-  console.log("User connected.");
-
-  const cookieHeader = req.headers.cookie;
-  const token = getTokenFromCookie(cookieHeader);
-  if (!token) return ws.close(4001, "No authentication cookie provided.");
-
-  try {
-    const decoded = jsonwebtoken.verify(token, JWT_SECRET);
-    ws.user = decoded;
-  } catch (error) {
-    return ws.close(4003, "Invalid or expired token.");
-  }
-
-  ws.on("message", (data, isBinary) => {
-    // Handle JOIN_ROOM, SEND_MSG and REC_MSG events.
-    const { type, room_url, content } = JSON.parse(data.toString("utf-8"));
-    if (!type || !room_url) return ws.send("Invalid request.");
-
-    switch (type) {
-      case "JOIN_ROOM": {
-        const participants = rooms.get(room_url);
-        if (participants) participants.add(ws);
-        else rooms.set(room_url, new Set([ws]));
-        ws.send(
-          JSON.stringify({
-            content: `User ${ws.user.username} joined successfuly.`,
-          }),
-        );
-        break;
-      }
-      case "SEND_MSG": {
-        if (!content) return;
-        let participants = rooms.get(room_url);
-        if (!participants) {
-          participants = new Set([ws]);
-          rooms.set(room_url, participants);
-        } else if (!participants.has(ws)) {
-          participants.add(ws);
-        }
-        const payload = {
-          type: "REC_MSG",
-          room_url,
-          content,
-          from: ws.user.username,
-          timeStamp: Date.now(),
-        };
-        participants.forEach((participant) => {
-          participant.send(JSON.stringify(payload));
-        });
-        break;
-      }
-      case "QUIT_ROOM": {
-        // to be implemented.
-      }
-    }
-  });
-
-  ws.on("close", (code, reason) => {
-    for (const [roomUrl, participants] of rooms) {
-      if (participants.has(ws)) {
-        participants.delete(ws);
-
-        if (participants.size === 0) {
-          rooms.delete(roomUrl);
-        }
-      }
-    }
-    console.log("User disconnected and removed from active room sets.");
-  });
-});
+initServer(server);
 
 app.get("/api/v1/", (req, res) => {
   return res.status(200).json({ success: true, message: "Hello, Web" });
---
+++ services/websocket.js
@@ -0,0 +1,87 @@
+import { WebSocketServer } from "ws";
+import jsonwebtoken from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "you-cant-guess-this";
+const rooms = new Map();
+let wss = null;
+
+function getTokenFromCookie(cookieHeader) {
+  if (!cookieHeader) return null;
+  const match = cookieHeader.match(/token=([^;]+)/);
+  return match ? match[1] : null;
+}
+
+export const initServer = (server) => {
+  wss = new WebSocketServer({ server });
+  wss.on("connection", (ws, req) => {
+    console.log("User connected.");
+
+    const cookieHeader = req.headers.cookie;
+    const token = getTokenFromCookie(cookieHeader);
+    if (!token) return ws.close(4001, "No authentication cookie provided.");
+
+    try {
+      const decoded = jsonwebtoken.verify(token, JWT_SECRET);
+      ws.user = decoded;
+    } catch (error) {
+      return ws.close(4003, "Invalid or expired token.");
+    }
+
+    ws.on("message", (data, isBinary) => {
+      // Handle JOIN_ROOM, SEND_MSG and REC_MSG events.
+      const { type, room_url, content } = JSON.parse(data.toString("utf-8"));
+      if (!type || !room_url) return ws.send("Invalid request.");
+
+      switch (type) {
+        case "JOIN_ROOM": {
+          const participants = rooms.get(room_url);
+          if (participants) participants.add(ws);
+          else rooms.set(room_url, new Set([ws]));
+          ws.send(
+            JSON.stringify({
+              content: `User ${ws.user.username} joined successfuly.`,
+            }),
+          );
+          break;
+        }
+        case "SEND_MSG": {
+          if (!content) return;
+          let participants = rooms.get(room_url);
+          if (!participants) {
+            participants = new Set([ws]);
+            rooms.set(room_url, participants);
+          } else if (!participants.has(ws)) {
+            participants.add(ws);
+          }
+          const payload = {
+            type: "REC_MSG",
+            room_url,
+            content,
+            from: ws.user.username,
+            timeStamp: Date.now(),
+          };
+          participants.forEach((participant) => {
+            participant.send(JSON.stringify(payload));
+          });
+          break;
+        }
+        case "QUIT_ROOM": {
+          // to be implemented.
+        }
+      }
+    });
+
+    ws.on("close", (code, reason) => {
+      for (const [roomUrl, participants] of rooms) {
+        if (participants.has(ws)) {
+          participants.delete(ws);
+
+          if (participants.size === 0) {
+            rooms.delete(roomUrl);
+          }
+        }
+      }
+      console.log("User disconnected and removed from active room sets.");
+    });
+  });
+};
